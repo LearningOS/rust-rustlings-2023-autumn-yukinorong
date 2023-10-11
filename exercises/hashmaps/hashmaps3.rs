6617b77588ac85d86d48d4550add2064a6b3377b
yukinorong <yukinorong@github.com>
2023-10-11 11:26:20 -0400

    total 3h 42min

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +38,23 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        if scores.contains_key(&team_1_name) {
+        } else {
+            scores.insert(team_1_name.clone(), Team{goals_scored:0, goals_conceded:0});
+        }
+        if let Some(team) = scores.get_mut(&team_1_name) {
+            team.goals_scored += team_1_score;
+            team.goals_conceded += team_2_score;
+        }
+
+        if scores.contains_key(&team_2_name) {
+        } else {
+            scores.insert(team_2_name.clone(), Team{goals_scored:0, goals_conceded:0});
+        }
+        if let Some(team) = scores.get_mut(&team_2_name) {
+            team.goals_scored += team_2_score;
+            team.goals_conceded += team_1_score;
+        }
     }
     scores
 }
