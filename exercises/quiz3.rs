a365f9cfded0fdec23f6379c575962ebe60de799
yukinorong <yukinorong@github.com>
2023-10-12 15:08:42 -0400

    total 8h 20min

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,7 +16,6 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub struct ReportCard {
     pub grade: f32,
@@ -24,12 +23,34 @@ pub struct ReportCard {
     pub student_age: u8,
 }
 
-impl ReportCard {
-    pub fn print(&self) -> String {
+trait NumericReport {
+    fn print(&self) -> String;
+}
+
+trait AlphabeticReport {
+    fn print(&self) -> String;
+}
+
+impl NumericReport for ReportCard {
+    fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
     }
 }
+
+impl AlphabeticReport for ReportCard {
+    fn print(&self) -> String {
+        format!("{} ({}) - achieved a grade of A+",
+            &self.student_name, &self.student_age)
+    }
+}
+
+// impl ReportCard {
+//     pub fn print(&self) -> String {
+//         format!("{} ({}) - achieved a grade of {}",
+//             &self.student_name, &self.student_age, &self.grade)
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -43,7 +64,7 @@ mod tests {
             student_age: 12,
         };
         assert_eq!(
-            report_card.print(),
+            NumericReport::print(&report_card),
             "Tom Wriggle (12) - achieved a grade of 2.1"
         );
     }
@@ -57,7 +78,7 @@ mod tests {
             student_age: 11,
         };
         assert_eq!(
-            report_card.print(),
+            AlphabeticReport::print(&report_card),
             "Gary Plotter (11) - achieved a grade of A+"
         );
     }
